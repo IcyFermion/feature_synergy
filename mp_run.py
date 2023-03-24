2cd42925a3e86d1b9a1f8e6fbce86161d7ade784
Bingran <shenbingran@gmail.com>
2023-03-23 22:14:14 -0400

    more exp

--- mp_run.py
+++ mp_run.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import RandomForestRegressor
 import numpy as np
+from xgboost import XGBRegressor
 
 import warnings
 from sklearn.exceptions import DataConversionWarning
@@ -8,7 +9,7 @@ warnings.filterwarnings("ignore")
 perturbation_factor = 3
 
 def rf_feature_importance(ts_train_X, ts_train_y, perturbation_input, i):
-    regr = RandomForestRegressor(random_state=i, warm_start=True, n_estimators=300, n_jobs=1)
+    regr = XGBRegressor(random_state=i, n_jobs=1)
     regr = regr.fit(ts_train_X, ts_train_y)
     input_mean = ts_train_X.mean()
     return regr.predict(np.array(input_mean).reshape(1,-1))[0] - regr.predict(perturbation_input)
