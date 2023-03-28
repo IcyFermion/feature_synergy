2a27e317e9e604de1f9075d13db96741e8ff0914
Bingran <shenbingran@gmail.com>
2023-03-28 09:51:11 -0400

    more exp

--- mp_run.py
+++ mp_run.py
@@ -12,8 +12,8 @@ warnings.filterwarnings("ignore")
 perturbation_factor = 3
 
 def rf_feature_importance(ts_train_X, ts_train_y, perturbation_input, perturbation_input_alt, i):
-    regr = XGBRegressor(random_state=i, n_jobs=1)
-    # regr = RandomForestRegressor(random_state=i, n_jobs=1)
+    # regr = XGBRegressor(random_state=i, n_jobs=1)
+    regr = RandomForestRegressor(random_state=i, n_jobs=1)
     # regr = XGBRFRegressor(random_state=i, n_jobs=1)
     # regr = AdaBoostRegressor(random_state=i)
     regr = regr.fit(ts_train_X, ts_train_y)
