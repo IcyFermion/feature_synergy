0034fed5552c35798dfffec280ecd9058f647426
Bingran Shen <shenbingran@gmail.com>
2023-04-10 18:42:10 -0400

    pairwise tf synergy test on arabidopsis

--- mp_run.py
+++ mp_run.py
@@ -20,3 +20,14 @@ def rf_feature_importance(ts_train_X, ts_train_y, perturbation_input, perturbati
     input_mean = ts_train_X.mean()
     base_predict = regr.predict(np.array(input_mean).reshape(1,-1))[0]
     return [base_predict - regr.predict(perturbation_input), base_predict - regr.predict(perturbation_input_alt)]
+
+
+def regr_perturbation(ts_train_X, ts_train_y, perturbation_input, perturbation_input_alt, i):
+    # regr = XGBRegressor(random_state=i, n_jobs=1)
+    regr = RandomForestRegressor(random_state=i, n_jobs=1)
+    # regr = XGBRFRegressor(random_state=i, n_jobs=1)
+    # regr = AdaBoostRegressor(random_state=i)
+    regr = regr.fit(ts_train_X, ts_train_y)
+    input_mean = ts_train_X.mean()
+    base_predict = regr.predict(np.array(input_mean).reshape(1,-1))[0]
+    return [regr.predict(perturbation_input) - base_predict, regr.predict(perturbation_input_alt) - base_predict]
